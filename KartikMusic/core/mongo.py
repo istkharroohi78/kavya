82422bd44ca24cff6ee60babefefc31fe00663d2
istkharroohi78 <istkharroohi@gmail.com>
2026-07-14 09:08:59 +0530

    Update mongo.py

--- KartikMusic/core/mongo.py
+++ KartikMusic/core/mongo.py
@@ -22,7 +22,7 @@ class MongoDB:
         Initialize the MongoDB connection.
         """
         self.mongo = AsyncMongoClient(config.MONGO_URL, serverSelectionTimeoutMS=12500)
-        self.db = self.mongo.Kartik
+        self.db = self.mongo.Anon
 
         self.admin_list = {}
         self.active_calls = {}
